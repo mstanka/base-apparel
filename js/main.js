b3e55d734528079df555db7365bbaece3542a619
mstanka <mice78.mm@gmail.com>
2019-09-07 18:54:33 +0200

    fixing js and responsivness

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 let email = document.getElementById("email");
 let emailValue = "";
 let regEx = /\S+@\S+\.\S+/;
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click", submitEmail);
+
+function submitEmail() {
+  if (validateEmail === true) 
+    document.querySelector("#submitted") = enabled;
+  }
 
 function validateEmail() {
   if (email) {
@@ -9,5 +17,9 @@ function validateEmail() {
   if (regEx.test(emailValue) !== true) {
     document.querySelector("#errorMessage").style.visibility = "visible";
     document.querySelector("#errorIcon").style.visibility = "visible";
-  } 
-}
+  } else 
+    return true;
+} 
+
+
+  
